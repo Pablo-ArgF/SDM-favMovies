770f24de624d5c1ce7d1ad726cac0b09efd9fd48
UO283216 <98907723+UO283216@users.noreply.github.com>
2023-09-26 11:20:43 +0200

    Initial commit

---
+++ app/src/main/java/com/example/favmovies/MainActivity.java
@@ -0,0 +1,131 @@
+package com.example.favmovies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Snackbar msgCreaCategoria;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //metemos los listeners despues de que se haga el setcontetview para asegurar
+        //de que el componente está en memoria
+        Button btnGuardar = findViewById(R.id.BtnGuardar);
+        btnGuardar.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                //validacion de campos
+                if(validarCampos()){
+                    Snackbar.make(findViewById(R.id.layoutPrincipal),"Elemento guardado",
+                            Snackbar.LENGTH_LONG)
+                            .show();
+                }
+            }
+        });
+
+        //boton de edicion
+        ImageButton btnEdit = findViewById(R.id.EditarBoton);
+        btnEdit.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Spinner spinner = findViewById(R.id.SpinnerCategoría);
+                if(spinner.getSelectedItemPosition() == 0){ //si esta sin categoria
+                    //TODO acabar esto en la clase
+                    /*msgCreaCategoria = Snackbar.make(findViewById(R.id.layoutPrincipal),
+                            R.string.msg_crearcategoria,Snackbar.LENGTH_LONG);*/
+                }
+                else {
+                    //TODO acabar esto en la clase
+                    /*msgCreaCategoria = Snackbar.make(findViewById(R.id.layoutPrincipal),
+                            R.string.msg_crear,Snackbar.LENGTH_LONG);*/
+                }
+            }
+        });
+
+    }
+
+    /**
+     * Valida que los campos del formulario han sido correctamente rellenados
+     * @return
+     */
+    private boolean validarCampos() {
+        boolean result = true;
+
+        EditText inputTitulo = findViewById(R.id.InputTitulo);
+        EditText inputArgumento = findViewById(R.id.InputArgumento);
+        EditText inputFecha = findViewById(R.id.FechaPelicula);
+        EditText inputDuracion = findViewById(R.id.DuracionPelicula);
+
+
+        //validamos que el titulo no esté vacio
+        String titulo = inputTitulo.getText().toString();
+        if(titulo.trim().isEmpty()){
+            inputTitulo.setError(getString(R.string.e_tituloInvalido));
+            result = false;
+        }
+        //hacemos lo mismo con el argumento de la pelicula
+        String argumento = inputArgumento.getText().toString();
+        if(argumento.trim().isEmpty()){
+            inputArgumento.setError(getString(R.string.e_descripcionInvalida));
+            result = false;
+        }
+        //validamos también la duración de la pelicula:
+        //  ha de tener un valor --> no puede estar vacía
+        //  ha de tener el formato HH:mm
+        String duracion = inputDuracion.getText().toString();
+        if(duracion.trim().isEmpty()){
+            inputDuracion.setError(getString(R.string.e_duracionInvalida_vacia));
+            result = false;
+        }
+        else {
+            //probamos a formatear el input a HH:mm
+            try {
+                Date time = new SimpleDateFormat("HH:mm").parse(duracion);
+            } catch (ParseException e) {
+                result = false;
+                inputDuracion.setError(getString(R.string.e_duracionInvalida_formato));
+            }
+        }
+
+        //validamos también la fecha de salida de la pelicula:
+        //  ha de tener un valor --> no puede estar vacía
+        //  ha de tener el formato dd/MM/yyyy
+        String fecha = inputFecha.getText().toString();
+        if(fecha.trim().isEmpty()){
+            inputFecha.setError(getString(R.string.e_fechaInvalida_vacia));
+            result = false;
+        }
+        else {
+            //probamos a formatear el input a dd/MM/yyyy
+            try {
+                Date fechaDate = new SimpleDateFormat("dd/MM/yyyy").parse(fecha);
+            } catch (ParseException e) { //si no nos permite formatearlo no cumple el formato correcto
+                result = false;
+                inputFecha.setError(getString(R.string.e_fechaInvalida_formato));
+            }
+        }
+
+        return result;
+    }
+}
